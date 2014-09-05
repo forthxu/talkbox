b04a88ed657bb45f7d72445de88b15ae371f2df1
forthxu <forthxu@gmail.com>
2014-09-05 08:06:44 +0800

    from company

--- src/uno/main.lua
+++ src/uno/main.lua
@@ -2,9 +2,12 @@ local skynet = require "skynet"
 
 local max_client = 64
 
+--增加读取lua文件的路径
+package.path = string.format("%s;%s?.lua;",   package.path, "./center/")
+
 skynet.start(function()
 	print("Server start")
-	skynet.newservice("talkbox")
+	skynet.newservice("center")
 	local watchdog = skynet.newservice("watchdog")
 	skynet.call(watchdog, "lua", "start", {
 		port = 10101,
