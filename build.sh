1229850270eeca787c7a74193ca55b2bdde8e495
徐福盛 <1072353388@qq.com>
2015-01-30 15:03:58 +0800

    update 添加build.sh 来生成gh-pages分支

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 gitbook init
 gitbook build --output=../talkbox_gh-pages
+cd ../talkbox_gh-pages
+git init
+git add .
+git commit -a -m "update 20150130"
+git remote add origin git@github.com:forthxu/talkbox.git
+git push --force origin master:gh-pages
+cd -
